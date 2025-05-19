4fff3c3fd89cb4adca47e1cc7f834f5591feb448
Colt Novak <colton.novak@students.coderva.org>
2025-05-19 14:22:00 -0400

    Update ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,15 @@
 return {
+    {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<leader>tt]],
+        direction = "horizontal",
+        size = 20
+      })
+    end
+  },
   {
     "folke/trouble.nvim",
     opts = { use_diagnostic_signs = true },
