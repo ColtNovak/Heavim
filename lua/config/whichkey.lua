b81746e5651b9697a75ae7a70b3f5ba9e15ace80
Colt Novak <colton.novak@students.coderva.org>
2025-05-22 09:02:31 -0400

    Update whichkey.lua

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 function M.setup()
-  require("which-key").setup()
+  require("which-key").setup({
+    notify = false
+  })
 
   require("binds.whichkeytele").setup()
   require("which-key.health").check()
-
 end
 
 return M
